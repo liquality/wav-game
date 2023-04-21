e60e63d1aa54039b6e42f3441ecf963b8990260f
Johanna Fransson <adrina0002@gmail.com>
2023-04-21 11:22:49 +0100

    add homepage btn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,9 +12,16 @@ export default function Home() {
             Collect, unlock tracks, chase early releases and swag, or step-up
             the game to win it all from your favorite artist.
           </p>
-          <p className="fontFamily">
+          <p className="mb-4 fontFamily">
             A fun burn-to-win NFT experience in 6 levels.
           </p>
+
+          <button
+            type="button"
+            class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900  headerBtn"
+          >
+            Start collecting
+          </button>
         </div>
       </div>
     </div>
