f39b945dec4f2ee780a5898ace15cceab7606f87
Johanna Fransson <adrina0002@gmail.com>
2023-05-08 17:08:44 +0100

    add game endpoints

--- backend/v1/endpoints.js
+++ backend/v1/endpoints.js
@@ -1,5 +1,7 @@
 var endpoints = {};
 var userHandler = require("./handlers/userHandler");
+var gameHandler = require("./handlers/gameHandler");
+
 var middleware = require("./middleware");
 
 endpoints.createUser = {
@@ -31,6 +33,39 @@ endpoints.deleteUser = {
   middleware: [],
   handler: userHandler.delete,
   description: "delete user",
+};
+/*  */
+//Game endpoints
+/*  */
+endpoints.createGame = {
+  url: "/v1/game",
+  method: "post",
+  middleware: [],
+  handler: gameHandler.create,
+  description: "create game",
+};
+endpoints.readGame = {
+  url: "/v1/game/:id",
+  method: "get",
+  middleware: [],
+  handler: gameHandler.read,
+  description: "create game",
+};
+
+endpoints.updateGame = {
+  url: "/v1/game/:id",
+  method: "put",
+  middleware: [],
+  handler: gameHandler.update,
+  description: "update game",
+};
+
+endpoints.deleteGame = {
+  url: "/v1/game/:id",
+  method: "delete",
+  middleware: [],
+  handler: gameHandler.delete,
+  description: "delete game",
 };
 
 module.exports = endpoints;
