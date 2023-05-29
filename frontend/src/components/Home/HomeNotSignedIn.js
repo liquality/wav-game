650b8985b5b6a1998936f9d370fe4811fc5eabd2
Johanna Fransson <adrina0002@gmail.com>
2023-05-29 13:57:29 +0100

    our vision is done

--- frontend/src/components/Home/HomeNotSignedIn.js
+++ frontend/src/components/Home/HomeNotSignedIn.js
@@ -13,6 +13,9 @@ import { ReactComponent as GlitterTwo } from "../../images/glitter_two.svg";
 import { ReactComponent as SparkleOne } from "../../images/sparkle_one.svg";
 import { ReactComponent as SparkleTwo } from "../../images/sparkle_two.svg";
 import { ReactComponent as Arrow } from "../../images/arrow_home.svg";
+import { ReactComponent as Winner } from "../../images/winner.svg";
+import { ReactComponent as RewardsTout } from "../../images/rewards_tout.svg";
+import { ReactComponent as DecoratedLine } from "../../images/decorated_line.svg";
 
 import { ArtistGrid } from "../ArtistGrid";
 
@@ -76,25 +79,65 @@ export default function HomeWhenNotSignedIn() {
             <div id="text">Level 1</div>
             <p className="levelCardText">From 1 artist, buy 32 collectables </p>
             <Union className="levelCardUnion" />
+            <Arrow
+              className="levelCardSvg"
+              style={{ left: "10%", bottom: "0%", top: "110%" }}
+            />
+            <p
+              className="levelCardSvg lightCoral text-xs"
+              style={{ left: "10%", bottom: "0%", top: "120%" }}
+            >
+              TRADE 1
+            </p>
           </div>
           <div className="ml-3" id="container">
             <div id="blockTwo"></div>
             <div id="text">Level 2</div>
             <p className="levelCardText">Get a top live Song</p>
             <Notes className="levelCardSvg" />
+            <Arrow
+              className="levelCardSvg"
+              style={{ left: "10%", bottom: "0%", top: "110%" }}
+            />
+            <p
+              className="levelCardSvg lightCoral text-xs"
+              style={{ left: "10%", bottom: "0%", top: "120%" }}
+            >
+              TRADE 2
+            </p>
           </div>
           <div className="ml-3" id="container">
             <div id="blockThree"></div>
             <div id="text">Level 3</div>
             <p className="levelCardText">Get an unreleased song</p>
-            <Cd className="levelCardSvg" />
+            <Cd className="levelCardSvg" />{" "}
+            <Arrow
+              className="levelCardSvg"
+              style={{ left: "10%", bottom: "0%", top: "110%" }}
+            />
+            <p
+              className="levelCardSvg lightCoral text-xs"
+              style={{ left: "10%", bottom: "0%", top: "120%" }}
+            >
+              TRADE 3
+            </p>
           </div>
           <div className="ml-3" id="container">
             <div id="blockFour"></div>
             <div id="text">Level 3</div>
             <p className="levelCardText">Get a limited physical item </p>
             <Microphone className="levelCardSvg" />
-            <Union className="levelCardUnion" style={{ left: "90%" }} />
+            <Union className="levelCardUnion" style={{ left: "90%" }} />{" "}
+            <Arrow
+              className="levelCardSvg"
+              style={{ left: "10%", bottom: "0%", top: "110%" }}
+            />{" "}
+            <p
+              className="levelCardSvg lightCoral text-xs"
+              style={{ left: "10%", bottom: "0%", top: "120%" }}
+            >
+              TRADE 4
+            </p>
           </div>
           <div className="ml-3" id="container">
             <div id="blockFive"></div>
@@ -102,7 +145,19 @@ export default function HomeWhenNotSignedIn() {
             <p className="levelCardText">
               Get a limited unreleased track performance
             </p>
+            <Arrow
+              className="levelCardSvg"
+              style={{ left: "10%", bottom: "0%", top: "110%" }}
+            />
+
+            <p
+              className="levelCardSvg lightCoral text-xs"
+              style={{ left: "10%", bottom: "0%", top: "120%" }}
+            >
+              TRADE 5
+            </p>
           </div>
+
           <div className="ml-3" id="container">
             <div id="blockSix"></div>
             <div id="text">Level 6</div>
@@ -123,13 +178,34 @@ export default function HomeWhenNotSignedIn() {
               className="levelCardSvg"
               style={{ left: "80%", top: "-10%" }}
             />
+            <Winner
+              className="levelCardSvg"
+              style={{ left: "30%", bottom: "0%", top: "110%" }}
+            />
           </div>
         </div>
+        <div className="flex justify-center items-center relative mt-5">
+          <RewardsTout className="mt-5" />
+
+          <div style={{ left: "34%", top: "35%" }} className="absolute">
+            <span className="lightCoral">
+              EXCLUSIVE REWARDS FOR <br></br> FULL SET HOLDERS
+            </span>
+          </div>
+          <div style={{ left: "34%", top: "58%" }} className="absolute">
+            <span className="text-xs">
+              Didn’t win the final prize? Don’t fret, as a full set <br></br>
+              holder you will still be rewarded.
+            </span>
+          </div>
+        </div>
+        <div className="mt-24"></div>
+        <br></br>
       </div>
 
-      <div className="m-auto text-center justify-center items-center ">
-        <div className="mt-5 lineCoral"></div>
-        <p className="webfont" style={{ fontSize: 64, color: "#FFA3DA" }}>
+      <div className="m-auto text-center justify-center items-center">
+        <div className="mt-5"></div>
+        <p className="webfont lightCoral" style={{ fontSize: "64px" }}>
           OUR VISION
         </p>
         <div className="m-auto" style={{ width: "50%" }}>
@@ -139,14 +215,16 @@ export default function HomeWhenNotSignedIn() {
             our experiments with the WRLD.
           </p>
         </div>
-        <div className="mt-3 flexDirectionRow">
-          <p className="text-xs lightPink mt-5  m-auto">CONNECT ON DISCORD</p>
+        <div className="mt-3 flex flex-row">
+          <p className="text-xs lightPink mt-5 m-auto">CONNECT ON DISCORD</p>
         </div>
         <SmallPinkArrow />
 
-        <br></br>
-        <div className=" mb-5 lineCoral"></div>
-        <br></br>
+        <br />
+        <div className="flex justify-center items-center relative mt-5 mb-5">
+          <DecoratedLine />
+        </div>
+        <br />
       </div>
     </div>
   );
