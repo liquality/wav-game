ebb8cfafde95458d75ff940290343d86c0e3e83d
Johanna Fransson <adrina0002@gmail.com>
2023-05-31 16:35:43 +0100

    if no env variables use local db

--- backend/MySQL.js
+++ backend/MySQL.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql2");
+var config = require("./config.json");
 
-var pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  database: process.env.MYSQLDATABASE,
-  password: process.env.MYSQLPASSWORD,
-  port: process.env.MYSQLPORT,
-});
+var pool = mysql.createPool(
+  !process.env.MYSQLDATABASE
+    ? config.database_connection
+    : {
+        host: process.env.MYSQLHOST,
+        user: process.env.MYSQLUSER,
+        database: process.env.MYSQLDATABASE,
+        password: process.env.MYSQLPASSWORD,
+        port: process.env.MYSQLPORT,
+      }
+);
 
 module.exports.pool = pool;
