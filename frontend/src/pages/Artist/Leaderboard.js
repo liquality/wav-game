cde78d2e7dba8948f78520a375c3e5ac1b4d5894
Johanna Fransson <adrina0002@gmail.com>
2023-06-05 18:35:39 +0100

    add opensea link

--- frontend/src/pages/Artist/Leaderboard.js
+++ frontend/src/pages/Artist/Leaderboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../../App.css";
 
 import "./artist.css";
+import { ReactComponent as SmallPinkArrow } from "../../images/small_pink_arrow.svg";
 
 const Leaderboard = ({ isOpen, onClose }) => {
   const [user, setUser] = React.useState({});
@@ -10,6 +11,10 @@ const Leaderboard = ({ isOpen, onClose }) => {
   const activeToggleStyle = {
     borderBottom: "1px solid #f251bc",
     color: "#f251bc",
+  };
+
+  const openSendModal = () => {
+    console.log("open send modal here");
   };
 
   const fetchUser = async () => {
@@ -76,6 +81,25 @@ const Leaderboard = ({ isOpen, onClose }) => {
       >
         <button onClick={() => setShowNfts(false)}>LEADERBOARD</button>
       </div>
+      {!showNfts ? null : (
+        <div className="flex absolute sendNfts   lightPink right-0">
+          <button onClick={openSendModal} className="sendNfts">
+            Send
+          </button>
+          <p style={{ color: "#bdbdbd" }} className="mr-3 ml-3">
+            |
+          </p>
+          <a
+            className="flex hover:no-underline lightPink  hover:text-decoration-none"
+            href="https://opensea.io/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View on Opensea <SmallPinkArrow className="ml-2 mt-2" />
+          </a>
+        </div>
+      )}
+
       <div
         style={showNfts ? activeToggleStyle : null}
         className="mt-3 mb-5 absolute nftsToggle"
