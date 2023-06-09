2be629cb4086929f45c6dda834c22a1f4df0cb41
Johanna Fransson <adrina0002@gmail.com>
2023-06-09 11:16:03 +0100

    add gameContract + nftContract in trade modal

--- frontend/src/components/Trade/TradeModal.js
+++ frontend/src/components/Trade/TradeModal.js
@@ -5,11 +5,23 @@ import { CompletedPayment } from "../Onboarding/CompletedPayment";
 import { CustomModal } from "../Modal";
 import { TradeStart } from "./TradeStart";
 import { ProcessingTrade } from "./ProcessingTrade";
+import { ethers } from "ethers";
+import {
+  WAV_NFT_ABI,
+  WAV_NFT_ADDRESS,
+  WAV_PROXY_ABI,
+  WAV_PROXY_ADDRESS,
+} from "../../data/contract_data";
+import { NftService } from "@liquality/wallet-sdk";
 
 export const TradeModal = (props) => {
   const { show, setShow } = props;
   const [content, setContent] = useState("tradeStart");
   const [headerText, setHeaderText] = useState("Trade");
+  const [nftContract, setNftContract] = useState(null);
+  const [gameContract, setGameContract] = useState(null);
+
+  const [wavNfts, setWavNfts] = useState(null);
 
   const [loading, setLoading] = useState(false);
 
@@ -17,10 +29,43 @@ export const TradeModal = (props) => {
   const handleShow = () => setShow(true);
 
   useEffect(() => {
-    const init = async () => {};
+    const initializeContract = async () => {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        // Create a new instance of the contract using the ABI and address
+        const nftContract = new ethers.Contract(
+          WAV_NFT_ADDRESS,
+          WAV_NFT_ABI,
+          provider
+        );
+        const gameContract = new ethers.Contract(
+          WAV_PROXY_ADDRESS,
+          WAV_PROXY_ABI,
+          provider
+        );
+        setNftContract(nftContract);
+        setGameContract(gameContract);
 
-    init();
-  }, [content]);
+        const nfts = await NftService.getNftsForContract(
+          WAV_NFT_ADDRESS,
+          80001
+        );
+        setWavNfts(nfts);
+      } catch (error) {
+        console.error("Error initializing contract:", error);
+      }
+    };
+
+    initializeContract();
+  }, []);
+
+  console.log(
+    wavNfts,
+    "wav nfts from alchemy",
+    nftContract,
+    "nft contract, proxy:",
+    gameContract
+  );
 
   const createNewWallet = async () => {
     setLoading(true);
