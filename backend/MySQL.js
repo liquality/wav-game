b350e139c4abef49a59bfad5f072c2818811e726
Johanna Fransson <adrina0002@gmail.com>
2023-06-27 10:52:07 +0200

    add logging

--- backend/MySQL.js
+++ backend/MySQL.js
@@ -1,6 +1,17 @@
 var mysql = require("mysql2");
 var config = require("./config.json");
 
+console.log(
+  config.database_connection,
+  "<-- local & CONFIG DB CONNECTION env variables:",
+  {
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    database: process.env.MYSQLDATABASE,
+    password: process.env.MYSQLPASSWORD,
+    port: process.env.MYSQLPORT,
+  }
+);
 var pool = mysql.createPool(
   !process.env.MYSQLDATABASE
     ? config.database_connection
