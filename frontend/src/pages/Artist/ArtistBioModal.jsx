fc80f25c6eaceda4899055d6f2e6bfe1ce38a73d
Johanna Fransson <adrina0002@gmail.com>
2023-06-28 14:20:38 +0200

    add readmore in artistbiomodal

--- frontend/src/pages/Artist/ArtistBioModal.jsx
+++ frontend/src/pages/Artist/ArtistBioModal.jsx
@@ -1,40 +1,42 @@
-
 import { CustomModal } from "../../components/Modal";
 
 export const ArtistBioModal = (props) => {
-    const { show, setShow, artist, image } = props;
-    const content = () => {
-        return (
-            <div className="container contentView">
-                <div className="flex flex-row mt-5">
-                    <div className="flex h-full items-center flex-col">
-                        <div className="flex flex-col px-5 gap-4">
-                            <div className="artist-image-bio" style={{backgroundImage: `url(${image})`}} />
-                            <div className="bio-artist-name">{artist.name}</div>
-                            <div className="bio-artist-desc">{artist.description}</div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-4 pr-5">
-                        <p>
-                            {artist.bio}
-                        </p>
-                        <p>
-                            {artist.quote}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        );
-    };
+  const { show, setShow, artist, image } = props;
+  const content = () => {
     return (
-        <CustomModal
-            show={show}
-            setShow={setShow}
-            content={content}
-            modalHeaderText={'Artist Bio'}
-        >
-            {" "}
-            { }
-        </CustomModal>
+      <div className="container contentView">
+        <div className="flex flex-row mt-5">
+          <div className="flex h-full items-center flex-col">
+            <div className="flex flex-col px-5 gap-4">
+              <div
+                className="artist-image-bio"
+                style={{ backgroundImage: `url(${image})` }}
+              />
+              <div className="bio-artist-name">{artist.name}</div>
+              <div className="bio-artist-desc">{artist.description}</div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pr-5">
+            <p>{artist.bio}</p>
+            <p>{artist.readMore}</p>
+            <p>{artist.quote}</p>
+            <br></br>
+            <div className="bio-artist-desc mt-4">FAVORITE GAMES:</div>
+            <p>{artist.favoriteGames}</p>
+          </div>
+        </div>
+      </div>
     );
+  };
+  return (
+    <CustomModal
+      show={show}
+      setShow={setShow}
+      content={content}
+      modalHeaderText={"Artist Bio"}
+    >
+      {" "}
+      {}
+    </CustomModal>
+  );
 };
