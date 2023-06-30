4d9500cd3f894c6eddea1b119bac6effd7851973
koderholic <22542946+koderholic@users.noreply.github.com>
2023-06-30 19:38:38 +0400

    refactor: move contract interactions to one file - add common hook useEarlyBirdCount - add socketPath to mysql config

---
+++ frontend/src/hooks/useEarlyBirdCount.js
@@ -0,0 +1,22 @@
+
+import { useEffect, useState } from "react";
+import ContractService from "../services/ContractService";
+
+export function useEarlyBirdInfo(gameId, level) {
+    const [earlyBirdCount, setEarlyBirdCount] = useState(null);
+    const [isEarlyBird, setIsEarlyBird] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const _earlyBirdCount = await ContractService.earlyBirdCount(gameId, level);
+          const _isEarlyBird = await ContractService.isEarlyBird(gameId, level);
+
+          setEarlyBirdCount(_earlyBirdCount);
+          setIsEarlyBird(_isEarlyBird);
+        };
+    
+        fetchData();
+      }, []);
+
+      return {earlyBirdCount, isEarlyBird};
+}
