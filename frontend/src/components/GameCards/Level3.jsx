34d9028bab3046bee0e806fe872ecf1d061dd1d5
Johanna Fransson <adrina0002@gmail.com>
2023-07-05 13:31:25 +0200

    revised dwaw 132

--- frontend/src/components/GameCards/Level3.jsx
+++ frontend/src/components/GameCards/Level3.jsx
@@ -14,24 +14,21 @@ export const Level3 = (props) => {
   const status = getLevelsStatuses(currentLevel || 1)[3];
   let instructions = "";
   let tradeActionText = "";
-  let actionDisbled = false;
+  let actionDisabled = false;
   let actionLocked = false;
+  let noActions = false;
 
-  actionDisbled = false;
+  actionDisabled = false;
   instructions = `You have ${level3Count === -1 ? 0 : level3Count} cards.`;
   if (level3Count < 2) {
     if (level3Count === 0) {
-      instructions = "You need 2 Artist cards to trade for this.";
-      tradeActionText = "Level locked";
+      instructions = "You have 0 cards. Trade 2 cards from the previous level.";
+      noActions = true;
+    } else if (level3Count === 1) {
+      instructions = `You have 1 card. Get 1 more from the previous level`;
       actionLocked = true;
-      actionDisbled = true;
-    } else {
-      instructions = `You have ${
-        level3Count === -1 ? 0 : level3Count
-      } cards. Trade 2 cards from the previous level to get one of these.`;
-
-      tradeActionText = "Start Trading";
-      actionDisbled = true;
+      tradeActionText = "Level Locked";
+      actionDisabled = true;
     }
   } else {
     if (burnStatus) {
@@ -41,16 +38,17 @@ export const Level3 = (props) => {
     }
   }
 
-  const actions = [
-    {
-      onActionClick: (level) => onTradeClick(level),
-      label: tradeActionText,
-      mode: actionLocked ? "pinkStroke" : "default",
-      disabled: actionDisbled,
-      useIcon: actionDisbled,
-    },
-  ];
-
+  const actions = noActions
+    ? []
+    : [
+        {
+          onActionClick: (level) => onTradeClick(level),
+          label: tradeActionText,
+          mode: actionLocked ? "pinkStroke" : "default",
+          disabled: actionDisabled,
+          useIcon: actionDisabled,
+        },
+      ];
   return (
     <LevelCard
       status={status}
@@ -62,7 +60,7 @@ export const Level3 = (props) => {
         title: "1 unreleased song + listening room",
         edition: "",
         instructions,
-        useEmtpyActionsStyle: false
+        useEmtpyActionsStyle: false,
       }}
     />
   );
