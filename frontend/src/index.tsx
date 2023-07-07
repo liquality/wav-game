3b2dd2e354d6d73200add973dd48dffd7fb5bcf0
Bradley Suira <bradleysuira@gmail.com>
2023-07-07 08:21:22 -0500

    chore: removed marker io widget

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,19 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import markerSDK from '@marker.io/browser';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const setupMarkerIO = async () => {
-  // const widget =
-  await markerSDK.loadWidget({
-    project: '64793ca543b16bd7907e7e39',
-  });
-}
 root.render(
   <BrowserRouter>
     <React.StrictMode>
@@ -27,4 +20,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-setupMarkerIO();
